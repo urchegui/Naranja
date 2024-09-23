57679e707e952a957ec2e5c57ad8dfe9ba5101e6
danielurchegui <daniel.urchegui@hotmail.com>
2024-09-23 17:24:41 +0200

    header mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/imagotipo_blanco.png";
 import "../styles/header.scss";
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="header-wrapper">
       <div className="header_logo">
         <Link to="/"><img src={Logo} alt="Naranaja Sound System" /></Link>
       </div>
-      <div className="header_links">
-        <HashLink smooth to="/#faqs">FAQS</HashLink>
-        <HashLink smooth to="/#entradas">Compra tu entrada</HashLink>
-        <Link to={"/about-us"}>Sobre Naranja</Link>
-        <HashLink smooth to="/#colaboradores">Colaboradores</HashLink>
+      
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </div>
+
+      <div className={`header_links ${menuOpen ? 'open' : ''}`}>
+        <HashLink smooth to="/#faqs" onClick={toggleMenu}>FAQS</HashLink>
+        <HashLink smooth to="/#entradas" onClick={toggleMenu}>Compra tu entrada</HashLink>
+        <Link to={"/about-us"} onClick={toggleMenu}>Sobre Naranja</Link>
+        <HashLink smooth to="/#colaboradores" onClick={toggleMenu}>Colaboradores</HashLink>
       </div>
     </div>
   );
