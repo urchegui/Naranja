e052c98ec360b902f42996c6ecdf26de555f262e
danielurchegui <daniel.urchegui@hotmail.com>
2024-09-25 18:11:50 +0200

    form

--- src/components/Colaboradores.jsx
+++ src/components/Colaboradores.jsx
@@ -1,19 +1,29 @@
 import Imbolc from "../assets/imbolcNegro.png"
 import Codex from "../assets/codex.png"
 import "../styles/colaboradores.scss";
+import { HashLink } from "react-router-hash-link";
 
 const Colaboradores = () => {
     return (
         <>
-        <div className="partners-wrapper" id="colaboradores">
-            <div className="partner-title">
-                <h1>APROVED BY</h1>
+            <div className="partners-wrapper" id="colaboradores">
+                <div className="partner-title">
+                    <h1>APROVED BY</h1>
+                </div>
+                <div className="logo-container">
+                    <img src={Codex} alt="Logo de Codex Sessions" />
+                    <img src={Imbolc} alt="Logo de Imbolc" id="imbolc" />
+                </div>
+                <div className="">
+                    <h2>¿Quieres colaborar con nosotros?</h2>
+                    <p>¡Mándanos un email con tu propuesta!</p>
+                    <HashLink to='#'
+                        onClick={(e) => {
+                            window.location.href = "mailto:info@naranjasoundsystem.com";
+                            e.preventDefault();
+                        }}>{"Envíanos tu propuesta :)"}</HashLink>
+                </div>
             </div>
-            <div className="logo-container">
-                <img src={Codex} alt="Logo de Codex Sessions" />   
-                <img src={Imbolc} alt="Logo de Imbolc" id="imbolc"/>
-            </div>
-        </div>
         </>
     )
 }
--- src/components/TicketPurchase.jsx
+++ src/components/TicketPurchase.jsx
@@ -1,47 +1,112 @@
 import React, { useState } from "react";
 import "../styles/form.scss";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { TbVinyl } from "react-icons/tb";
 
 const TicketPurchase = () => {
   const [isChecked, setIsChecked] = useState(false);
+  const [buttonText, setButtonText] = useState("Comprar Entrada");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-  // Manejar el cambio del checkbox
+  const form = useRef();
+
+  const sendEmail = () => {
+    console.log("queso")
+    emailjs
+      .sendForm('service_nguzaoq', 'template_xnzxnie', form.current, {
+        publicKey: 'sL-s-Yl-QfLO6LKkT',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   const handleCheckboxChange = (event) => {
     setIsChecked(event.target.checked);
   };
 
-  // Manejar la compra de entradas
   const handlePurchase = () => {
     if (isChecked) {
-      alert("¡Gracias por aceptar! Ahora puedes proceder a comprar tu entrada.");
+      setButtonText("Te contactaremos");
+      setIsButtonDisabled(true);
+      sendEmail();
     }
   };
 
   return (
     <div className="purchase-wrapper">
       <h2>Compra tu entrada</h2>
-      <p>
-        Antes de continuar con la compra de tu entrada, por favor acepta nuestros
-        <strong> términos y condiciones</strong>. Es importante que leas atentamente la información relacionada con el evento, política de reembolsos y las normas de conducta.
-      </p>
-      
-      {/* Casilla de verificación */}
-      <label className="checkbox-container">
-        <input 
-          type="checkbox" 
-          checked={isChecked} 
-          onChange={handleCheckboxChange} 
+
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="label-wrapper">
+          <label>Nombre</label>
+          <input type="text" name="user_name" required />
+        </div>
+        <div className="label-wrapper">
+          <label>Apellido</label>
+          <input type="text" name="user_lastname" required />
+        </div>
+        <div className="label-wrapper">
+          <label>DNI</label>
+          <input type="text" name="user_dni" required />
+        </div>
+        <div className="label-wrapper">
+          <label>Correo</label>
+          <input type="email" name="user_email" required />
+        </div>
+        <div className="label-wrapper">
+          <label>Teléfono</label>
+          <input type="tel" name="user_phone" required />
+        </div>
+        <div className="label-wrapper">
+          <label>Alcohol</label>
+          <select name="user_alcohol" id="alcohol" required>
+            <option value="" selected disabled>Selecciona tu bebida</option>
+            <option value="ron">Ron</option>
+            <option value="whisky">Whisky</option>
+            <option value="vodka">Vodka</option>
+            <option value="gin">Gin</option>
+            <option value="Nada">Nada</option>
+
+          </select>
+        </div>
+        <div className="label-wrapper">
+          <label>Cerveza o tinto</label>
+          <select name="user_beer" id="cerveza" required>
+            <option value="" selected disabled>Selecciona tu bebida</option>
+            <option value="Cerveza">Cerveza</option>
+            <option value="Tinto">Tinto</option>
+            <option value="Nada">Nada</option>
+          </select>        </div>
+        <div className="label-wrapper">
+          <label>¿Algo a tener en cuenta? ¡No seas tímido!</label>
+          <textarea name="user_message" required />
+        </div>
+
+    
+        <label className="checkbox-container">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            required
+          />
+          Acepto ser grabado * Usted comprende que acepta ser grabado para la distribución de Naranja Sound System y que su voz y su imagen pueden usarse para promover futuros eventos.
+        </label>
+        <input
+          type="submit"
+          className="purchase-button"
+          onClick={handlePurchase}
+          disabled={!isChecked || isButtonDisabled}
+          value={buttonText}
         />
-        Acepto los términos y condiciones.
-      </label>
-      
-      {/* Botón de compra deshabilitado hasta que se marque la casilla */}
-      <button 
-        className="purchase-button" 
-        onClick={handlePurchase} 
-        disabled={!isChecked}
-      >
-        Comprar Entrada
-      </button>
+      </form>
     </div>
   );
 };
